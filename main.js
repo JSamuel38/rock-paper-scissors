563a1b590b8d1867c3cf9a1c3a268a214daf8792
Samuel Evans <117552758+JSamuel38@users.noreply.github.com>
2023-01-02 16:10:05 +0000

    Add round function for win case logic

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 function getComputerChoice() {
     let rand = Math.floor(Math.random() * 3);
-    console.log(rand);
     if (rand === 0) {
         return "rock";
     } else if (rand === 1) {
@@ -8,4 +7,34 @@ function getComputerChoice() {
     } else {
         return "scissors";
     }
+}
+
+function round(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    switch (playerSelection) {
+        case "rock":
+            if (computerSelection === "scissors") {
+                return "You win!";
+            } else if (computerSelection === "paper") {
+                return "You lose!";
+            }
+            break;
+
+        case "paper":
+            if (computerSelection === "rock") {
+                return "You win!";
+            } else if (computerSelection === "scissors") {
+                return "You lose!";
+            }
+            break;
+        
+        case "scissors":
+            if (computerSelection === "paper") {
+                return "You win!";
+            } else if (computerSelection === "rock") {
+                return "You lose!";
+            }
+            break;
+    }
+    return "It's a tie!";
 }
